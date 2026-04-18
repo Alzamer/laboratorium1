d25c91065026abbd866e24f7e11260107f174cf8
Alzamer <91850532+Alzamer@users.noreply.github.com>
2026-04-18 13:08:51 +0200

    Laboratorium 1 (#1)
    * Drugi commit
    
    * zadanie 5

--- tb/top.sv
+++ tb/top.sv
@@ -1,7 +1,14 @@
-module top(input clk);
-  dut instance1(clk);
-  top_tb instance2(clk);
-initial begin
-  $display("Hello World!");
-end
+module top ();
+    logic clk_sig;
+    logic rstn_sig;
+
+    dut instance1 (
+        .clk(clk_sig),
+        .rstn(rstn_sig)
+    );
+
+    top_tb instance2 (
+        .clk(clk_sig),
+        .rstn(rstn_sig)
+    );
 endmodule
