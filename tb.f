8b5f8450402367b81a68e4c95e4328b24e470af4
alzamer <mic.szendzielorz@gmail.com>
2026-07-08 23:55:23 +0200

    Krok 3

--- tb.f
+++ tb.f
@@ -1,2 +1,3 @@
 tb/top.sv
+tb/my_uvm_pkg.sv
 tb/top_tb.sv
---
+++ tb/my_uvm_pkg.sv
@@ -0,0 +1,12 @@
+`include "uvm_macros.svh"
+
+package my_uvm_pkg;
+  import uvm_pkg::*;
+  `include "myprefix_defines.svh" 
+  `include "myprefix_sequence_item.sv"
+  `include "seq/myprefix_base_seq.sv"
+  `include "myprefix_sequencer.sv"
+  `include "myprefix_driver.sv"
+  `include "myprefix_env.sv"
+  `include "tests/myprefix_base_test.sv"
+endpackage
