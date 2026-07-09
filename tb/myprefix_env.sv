f6cb1358d3f65bd0725eaeada7ba9c0cb0545f05
alzamer <mic.szendzielorz@gmail.com>
2026-07-09 20:35:21 +0200

    Krok 5

--- tb/myprefix_env.sv
+++ tb/myprefix_env.sv
@@ -4,9 +4,8 @@ class myprefix_env extends uvm_env;
     myprefix_driver     m_drv;
     myprefix_sequencer  m_seqr;
     myprefix_monitor    m_mon;
-    
     myprefix_scoreboard m_scb;
-    
+    myprefix_coverage   m_cov;
     myprefix_config     m_cfg;
 
     function new(string name, uvm_component parent);
@@ -26,6 +25,10 @@ class myprefix_env extends uvm_env;
         if (m_cfg.scoreboard_enable) begin
             m_scb = myprefix_scoreboard::type_id::create("m_scb", this);
         end
+
+	if (m_cfg.coverage_enable) begin
+            m_cov = myprefix_coverage::type_id::create("m_cov", this);
+        end
     endfunction
 
     function void connect_phase(uvm_phase phase);
@@ -34,6 +37,10 @@ class myprefix_env extends uvm_env;
         
         if (m_cfg.scoreboard_enable) begin
             m_mon.ap.connect(m_scb.ap_imp);
+        end
+
+	if (m_cfg.coverage_enable) begin
+            m_mon.ap.connect(m_cov.analysis_export);
         end
     endfunction
 endclass
