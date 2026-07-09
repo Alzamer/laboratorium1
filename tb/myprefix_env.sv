d309d24fcc0efc7e0e2866de42d71478502eaca7
alzamer <mic.szendzielorz@gmail.com>
2026-07-09 19:02:07 +0200

    Krok 2

--- tb/myprefix_env.sv
+++ tb/myprefix_env.sv
@@ -3,6 +3,7 @@ class myprefix_env extends uvm_env;
     
     myprefix_driver    m_drv;
     myprefix_sequencer m_seqr;
+    myprefix_monitor m_mon;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -12,6 +13,7 @@ class myprefix_env extends uvm_env;
         super.build_phase(phase);
         m_drv  = myprefix_driver::type_id::create("m_drv", this);
         m_seqr = myprefix_sequencer::type_id::create("m_seqr", this);
+        m_mon = myprefix_monitor::type_id::create("m_mon", this);
     endfunction
 
     function void connect_phase(uvm_phase phase);
